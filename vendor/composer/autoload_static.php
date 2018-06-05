e3fcdeb3eed0117e84ed00e58b688bff00688a06
QianLei <qianlei@icolumn.cn>
2018-06-05 13:52:48 +0800

    增加phpexcel库

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -32,11 +32,22 @@ class ComposerStaticInit3f2e03be225ec5fa643a81537dc59a4e
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'P' => 
+        array (
+            'PHPExcel' => 
+            array (
+                0 => __DIR__ . '/..' . '/phpoffice/phpexcel/Classes',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit3f2e03be225ec5fa643a81537dc59a4e::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit3f2e03be225ec5fa643a81537dc59a4e::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit3f2e03be225ec5fa643a81537dc59a4e::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
